3d60ee83ed58a05da22cb5b88eb444d32ef1b960
vamsi <vamsikumarsarma@gmail.com>
2020-12-10 21:04:35 +0530

    changes

--- Bit_Manipulations/basics.c
+++ Bit_Manipulations/basics.c
@@ -3,14 +3,16 @@
 
 #define ISODD(n) ((n & 1))
 #define ISPOW(n) (n & (n-1))
+#define TOGGLE(num, pos) num ^= (1<<(pos-1))
 int main(){
 
-	int ch,num;
+	int ch,num,pos;
 	while(1){
 		printf("\n***Bit operations to perform***\n");
 		printf("1.check EVEN or ODD Number\n");
 		printf("2. check powers of 2\n");
-		printf("3.exit\n");
+		printf("3.Toggle a bit\n");
+		printf("4.exit\n");
 		printf("Enter a choice\n");
 		scanf("%d",&ch);
 
@@ -35,7 +37,18 @@ int main(){
 						printf("%d is not a power of 2\n",num);
 					}
 					break;
-			case 3: exit(0);
+			case 3 : 
+	
+					 printf("Enter a number\n");
+					 scanf("%d",&num);
+					 printf("Enter a position toggle\n");
+					 scanf("%d",&pos);
+					 printf("number before toggle is %d\n",num);
+					 TOGGLE(num,pos);
+					 printf("number before toggle is %d\n",num);
+					 break;
+
+			case 4: exit(0);
 
 					
 
--- Bit_Manipulations/swap_even_odd.c
+++ Bit_Manipulations/swap_even_odd.c
@@ -3,7 +3,7 @@ int main(){
 	int x;
 	printf("Enter a number\n");
 	scanf("%d",&x);
-        x = ( (x & 0xF0F0F0F0)>>4 | (x & 0x0F0F0F0F)<<4 );
+        x = ( (x & 0xF0F0F0F0)>>1 | (x & 0x0F0F0F0F)<<1 );
         printf("the number is %d\n",x);
 	return 0;
 }
